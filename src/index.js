c0425809159f5b3668367174074d0c881ed26899
Alezx311 <alezx311@gmail.com>
2021-03-16 13:57:56 +0200

    rewrite package with TypeScript, create CommonJS and ES2020 modules, edit README

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const Constants = require("./Constants");
-const Random = require("./Random");
-const Music = require("./Music");
-const Values = require("./Values");
-const FileSystem = require("./FileSystem");
-
-module.exports = { Constants, Random, Music, FileSystem, Values };
